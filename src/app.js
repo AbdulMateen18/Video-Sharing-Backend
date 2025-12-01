05676590c1ef53dacae22fdae13fc1e528774236
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2025-12-02 02:33:22 +0500

    controller added for tweet

--- src/app.js
+++ src/app.js
@@ -20,10 +20,12 @@ app.use(cookieParser());
 import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
 
 // routes declaration
 app.use("/api/v1/users", userRouter); // http://localhost:5000/api/v1/users/register
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/tweets", tweetRouter);
 
 export { app };
